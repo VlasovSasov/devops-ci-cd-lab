c89d164696183b7588b850627a609637e27dce6e
Vlasov <drenskikh@mail.ru>
2025-11-30 07:54:13 +0000

    Work ne Volk - ne ubezhit 3

--- server/application.py
+++ server/application.py
@@ -1,16 +1,23 @@
+"""
+For Flask web server
+"""
+
 import http.server
 import socketserver
 
 PORT = 8000
 
 class TestMe():
+    """ Dai 5!"""
     def take_five(self):
         return 5
 
+    """ Nomer porta"""
     def port(self):
         return PORT
 
 if __name__ == '__main__':
+    """Main.com"""
     Handler = http.server.SimpleHTTPRequestHandler
     with socketserver.TCPServer(("", PORT), Handler) as http:
         print("Port Vlasova: ", PORT)
